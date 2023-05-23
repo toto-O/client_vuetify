4c977590a19635a18db062c4d27c9c15ff73ebe6
toto-O <phacusr3tr4@icloud.com>
2023-05-23 18:10:27 +0900

    add v-model component , InputTextBox

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <!-- <HelloWorld /> -->
+      <v-container>
+        <v-card>
+          <v-app-bar></v-app-bar>
+          <InputTextBox v-model="input_value" />
+          {{ input_value }}
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
+// import HelloWorld from '@/components/HelloWorld.vue'
+import InputTextBox from "@/components/InputTextBox.vue";
+
+const input_value = ref<string>("hello");
 </script>
---
+++ src/components/InputTextBox.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-text-field label="label" v-model="update" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+const props = defineProps(["value"]);
+const emit = defineEmits(["input"]);
+
+const update = computed({
+  get() {
+    return props.value;
+  },
+  set() {
+    emit("input", update);
+  },
+});
+</script>
